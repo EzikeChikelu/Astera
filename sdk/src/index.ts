d8bb2d0be0149675199897189ed9bb983097d764
Dannyswiss1 <dannyswiss9ija@gmail.com>
2026-06-26 22:20:25 +0100

    feat(sdk+ci): export contract errors, run integration tests, add cargo+npm audit

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -1,3 +1,7 @@
 export * from './client';
 export * from './types';
 export * from './stellar';
+export { Errors as InvoiceErrors } from './generated/invoice';
+export { Errors as PoolErrors } from './generated/pool';
+export { Errors as CreditScoreErrors } from './generated/credit_score';
+export { GovernanceError } from './generated/governance';
